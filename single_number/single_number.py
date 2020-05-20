2fceb39b6d44748a2980a73acffecc3c954bcce6
Wilma <kima_92@fastmail.com>
2020-05-20 16:14:53 -0400

    Implemented single_number.py. It passes the Test.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,40 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # Set avariable to store duplicates
+    duplicates = []
+    # Set variable to store the unique element
+    uniques = []
 
-    pass
+    # For each element in arr
+    for i in arr:
+
+        # If uniques is not empty
+        if uniques != []:
+            
+            # If the element is not in unique
+            if i not in uniques:
+                # Add the element in uniques
+                uniques.append(i)
+
+
+            # Else: if the element is already in uniques
+            else:
+                # Store in duplicates
+                duplicates.append(i)
+                # Remove the old one from unique,
+                # and add it to duplicates
+                uniques.remove(i)
+                duplicates.append(i)
+
+        # Else: If unique is empty
+        else:
+            # Add the element to uniques
+            uniques.append(i)
+    
+    # Return the first element in unique
+    return uniques[0]
+
 
 
 if __name__ == '__main__':
