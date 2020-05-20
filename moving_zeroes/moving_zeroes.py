9c61a10c32b007c97d65e9d1d1b0aceff39c36f0
Wilma <kima_92@fastmail.com>
2020-05-20 16:24:03 -0400

    Implemented moving_zeros.py. It successfully passes the test.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,30 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # Set a variable for the new array result
+    result = []
+    # Set a variable to store the zeros
+    zeros = []
 
-    pass
+    # For each element in arr
+    for i in arr:
+        # If the element is 0
+        if i == 0:
+            # Store in zeros
+            zeros.append(i)
+        # Else
+        else:
+            # Store in result
+            result.append(i)
+
+    # If zeros is not empty
+    if len(zeros) != 0:
+        # Append each of it's elements to result
+        result.extend(zeros)
+
+    # Return the new array result
+    return result
+
 
 
 if __name__ == '__main__':
